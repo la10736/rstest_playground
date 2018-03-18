93c9d66306b8007fa4daf1d4b31ba85070205abd
michele <michele.damico@gmail.com>
2018-03-18 16:07:33 +0100

    Add (commented) target code

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,25 @@ fn other(_s: &str, _v: i32) {
 fn main() {
     other("pippo", 42);
 }
+
+// You should implement rstest procedural macro to run `some_test` as cargo test by
+// using `fixture()` and `fix_string()` fixtures
+//
+
+//extern crate rstest;
+//
+//use rstest::rstest;
+//
+//fn fixture() -> &'static str {
+//    "42"
+//}
+//
+//fn fix_string() -> String {
+//    "String".to_string()
+//}
+//
+//#[rstest]
+//fn some_test(fixture: &str, fix_string: String) {
+//    assert_eq!(fixture, "42");
+//    assert_eq!(fix_string, "String".to_string());
+//}
